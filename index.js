ac1e817123944160b2da6e6f6b40bc05673a8901
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-12-31 15:28:53 +0530

    up for beta testing

--- index.js
+++ index.js
@@ -384,5 +384,5 @@ process.on('SIGINT', async () => {
   }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
